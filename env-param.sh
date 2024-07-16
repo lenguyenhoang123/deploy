f3a736cb05ba50b98c31091df2c5f8236978b2cf
minhtrieu <minhtrieu@meu-solutions.com>
2024-07-16 14:02:04 +0700

    [tag]staging

---
+++ env-param.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+export project_name="shtt-online-exam-be"
+export image_name="registry.gitlab.com/meucicd/shtt-online-exam-be"
+export port_mapping="3031"
+export environment_name="staging"
+export mount_data_folder="/mnt/data"
+export img_bak_tag="old"
+export runner_builder_folder="/home/gitlab-runner/builds/${RUNNER_TOKEN_PROD}"
+
+export release_version="1.0.0"
+
+
+
+
