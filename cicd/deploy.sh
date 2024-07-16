7c4099fe3110d93f087394716666cc5b12c558b5
Triệu Quốc Minh <minhtrieu@meu-solutions.com>
2024-07-16 14:30:17 +0700

    add env

--- cicd/deploy.sh
+++ cicd/deploy.sh
@@ -6,6 +6,7 @@ sed -i 's|IMAGE-NAME|'$image_name'|' docker-compose.yaml
 sed -i 's|IMAGE-TAG|'$tag'|' docker-compose.yaml
 sed -i 's|P-MAPPING|'$port_mapping'|' docker-compose.yaml
 sed -i 's|MOUNT-DATA-FOLDER|'$mount_data_folder'|' docker-compose.yaml
+sed -i 's|NODE_ENV_VALUE|'$node_env_value'|' docker-compose.yaml
 echo "$CI_REGISTRY_PW" | sudo docker login registry.gitlab.com -u "$CI_REGISTRY_USER" --password-stdin
 sudo docker tag $image_name:$tag  $image_name:$img_bak_tag
 sudo docker compose pull
--- env-param.sh
+++ env-param.sh
@@ -6,7 +6,7 @@ export environment_name="staging"
 export mount_data_folder="/mnt/data"
 export img_bak_tag="old"
 export runner_builder_folder="/home/gitlab-runner/builds/${RUNNER_TOKEN_PROD}"
-
+export node_env_value="staging"
 export release_version="1.0.0"
 
 
