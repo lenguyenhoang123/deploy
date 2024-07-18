ffb8daee7a2f94f94b55bc90026208ecf914113f
longnguyen <longnguyen@meu-solutions.com>
2024-07-18 14:45:22 +0700

    [tag]staging

--- src/services/data-handlers/validatorService.ts
+++ src/services/data-handlers/validatorService.ts
@@ -1,57 +1,5 @@
 import { check, body } from "express-validator";
 
-export const validateEmail = () => [
-	body("email", "Email không được để trống").not().isEmpty(),
-
-	body("email", "Email không hợp lệ").isEmail(),
-];
-
-export const validateRegisterUser = () => {
-	return [
-		body("full_name", {
-			vi: "Tên không được để trống",
-			en: "Name can not be empty",
-		})
-			.not()
-			.isEmpty(),
-
-		body("phone", {
-			vi: "Số điện thoại không được để trống",
-			en: "Phone number can not be empty",
-		})
-			.not()
-			.isEmpty(),
-		body("phone", {
-			vi: "Số điện thoại không hợp lệ",
-			en: "Phone number is invalid",
-		}).isMobilePhone("vi-VN"),
-
-		body("password", {
-			vi: "Mật khẩu không được để trống",
-			en: "Password can not be empty",
-		})
-			.not()
-			.isEmpty(),
-		body("password", {
-			vi: "Mật khẩu không thể chứa khoảng trắng",
-			en: "Password can not contain spaces",
-		})
-			.not()
-			.contains(" "),
-		body("password", {
-			vi: "Mật khẩu cần ít nhất 8 ký tự",
-			en: "Password needs at least 8 characters",
-		}).isLength({ min: 8 }),
-
-		// check("re_password")
-		//   .isLength({ min: 1 })
-		//   .withMessage("Xác nhận mật khẩu bắt buộc nhập"),
-
-		// check('birthday', 'Invalid birthday').isISO8601('yyyy-mm-dd'),
-		//  check('password', 'password more than 6 degits').isLength({ min: 6 })
-	];
-};
-
 export const validateLogin = () => {
 	return [
 		check("email", "Email không được để trống").not().isEmpty(),
@@ -70,6 +18,49 @@ export const validateForgetPassword = () => {
 		check("email", "Email tối đa 50 ký tự").isLength({ max: 50 }),
 	];
 };
+
+export const validateRegisterUser = () => [
+	...validateEmail(),
+	...validateFirstName(),
+	...validateMiddleName(),
+	...validateLastName(),
+	...validatePhone(),
+	...validateUnit(),
+	...validatePassword(),
+];
+
+export const validateEmail = () => [
+	body("email", "Email không được để trống").not().isEmpty(),
+	body("email", "Email không hợp lệ").isEmail(),
+];
+
+export const validateFirstName = () => [
+	body("first_name", "Tên không được để trống").not().isEmpty(),
+	body("first_name", "Tên không hợp lệ").matches(/^[a-zA-ZÀ-ỹ\s]+$/),
+];
+
+export const validateMiddleName = () => [body("middle_name", "Tên lót không hợp lệ").matches(/^[a-zA-ZÀ-ỹ\s]+$/)];
+
+export const validateLastName = () => [
+	body("last_name", "Họ không được để trống").not().isEmpty(),
+	body("last_name", "Họ không hợp lệ").matches(/^[a-zA-ZÀ-ỹ\s]+$/),
+];
+
+export const validatePhone = () => [
+	body("phone", "Số điện thoại không được để trống").not().isEmpty(),
+	body("phone", "Số điện thoại không hợp lệ").isMobilePhone("vi-VN"),
+];
+
+export const validateUnit = () => [
+	body("unit.district", "Quận không được để trống").not().isEmpty(),
+	body("unit.ward", "Phường/ xã không được để trống").not().isEmpty(),
+];
+
+export const validatePassword = () => [
+	body("password", "Mật khẩu không được để trống").not().isEmpty(),
+	body("password", "Mật khẩu không thể chứa khoảng trắng").not().contains(" "),
+	body("password", "Mật khẩu cần ít nhất 8 ký tự").isLength({ min: 8 }),
+];
 
 export default {
 	validateEmail,
