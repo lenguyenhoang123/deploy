d6fd5c59e2bfea7832e4eb2e040372a5d1d2abd0
Quế Nguyệt <quentn0620@gmail.com>
2024-08-06 11:46:50 +0700

    Add attribute created_at for api GET /user

--- src/controllers/api/v1.0/user/index.ts
+++ src/controllers/api/v1.0/user/index.ts
@@ -66,7 +66,17 @@ export default (_express: Application) => {
 						currentPage: req.payload.currentPage,
 						sortField: req.payload.sortField,
 						sortOrder: req.payload.sortOrder,
-						attributes: ["first_name", "middle_name", "last_name", "email", "phone", "unit", "is_active", "is_admin"],
+						attributes: [
+							"first_name",
+							"middle_name",
+							"last_name",
+							"email",
+							"phone",
+							"unit",
+							"is_active",
+							"is_admin",
+							"created_at",
+						],
 					};
 
 					return res.sendOk({
