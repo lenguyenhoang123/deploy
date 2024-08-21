d38b71bf3037a417cfac5ff500574f0cfaeb943e
Quế Nguyệt <quentn0620@gmail.com>
2024-08-21 13:49:09 +0700

    Modify API begin/submit exam: Retrieve start_time and submit_time from frontend

--- src/controllers/api/v1.0/user/exam/{exam_id}/begin.ts
+++ src/controllers/api/v1.0/user/exam/{exam_id}/begin.ts
@@ -42,40 +42,36 @@ export default (_express: Application) => {
 
 				try {
 					const currentTime = new Date();
-					const userId = await userProvider.getUserIdFromRequest(req);
 
 					const examId = req.params.exam_id as string;
-					if (!examId) throw new Error("Exam ID không được để trống");
-					if (!mongoose.Types.ObjectId.isValid(examId)) throw new Error("Exam ID không hợp lệ");
-
-					const exam = await provider.getById(examId);
-					if (!exam) throw new Error("Kỳ thi không tồn tại");
+					const exam = await provider.validateAndFetchExam(examId);
 
 					if (currentTime < exam.start_time) throw new Error("Kỳ thi chưa diễn ra");
 					if (currentTime > exam.end_time) throw new Error("Kỳ thi đã hết hạn");
 
-					const participant = exam.participants.find((p) => p.user_id.toString() === userId.toString());
+					const userId = await userProvider.getUserIdFromRequest(req);
+
+					let participant = exam.participants.find((p) => p.user_id.toString() === userId.toString());
 					if (!participant) throw new Error("Bạn chưa đăng ký kỳ thi này");
 
 					if (participant.submit_time) throw new Error("Bạn đã hoàn thành bài thi. Không thể bắt đầu.");
 
 					if (!exam.template) throw new Error("Không tìm thấy danh sách câu hỏi của kỳ thi.");
 
-					// Update Exam's Participants
-					const remainingParticipants = exam.participants.filter((p) => p.user_id.toString() !== userId.toString());
-					let updatedParticipants = remainingParticipants;
-					let updatedParticipant = participant;
+					// Shuffle Questions And Answers For Participant
 					if (!participant.answers || participant.answers.length === 0) {
-						updatedParticipant.answers = await provider.getShuffleQuestionsAndAnswers(examId);
+						participant.answers = await provider.getShuffleQuestionsAndAnswers(examId);
+
+						const remainingParticipants = exam.participants.filter((p) => p.user_id.toString() !== userId.toString());
+						const updatedParticipants = [...remainingParticipants, participant];
+
+						const data = await exam.updateOne({
+							participants: updatedParticipants,
+						});
+
+						if (data.modifiedCount <= 0) throw new Error("Có lỗi xảy ra khi bắt đầu bài thi");
 					}
-					updatedParticipant.start_time = currentTime;
-					updatedParticipants.push(updatedParticipant);
 
-					const data = await exam.updateOne({
-						participants: updatedParticipants,
-					});
-
-					if (data.modifiedCount <= 0) throw new Error("Có lỗi xảy ra khi bắt đầu bài thi");
 					return res.sendOk({ data: { message: "Bắt đầu làm bài thi thành công" } });
 				} catch (error) {
 					return res.sendError({ err: error });
